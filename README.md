70338cec09f12b4865c8f2d1018d2d724d084b4b
Herr Pinguinito <176357385+DerPinguinito@users.noreply.github.com>
2024-12-05 12:28:28 -0300

    Creates game board using IIFE and factory functions
    The BOARD function creates and returns a unique grid instance.
    
    Within the function a combination of three private function were added
    to create the rows, the columns and the grid, respectively.
    
    The function returns only the grid array.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ttt
+# Tic Tac Toe
+
+The goal of the project is to create a Tic Tac Toe webpage
+with as little global code as possible. The code should display
+use of module patterns and each section of it should be logically
+placed in a way that results organic or intuitive to navigate.
---
+++ main.js
@@ -0,0 +1,38 @@
+
+const BOARD = function() {
+    const grid =  [];
+
+    const createRow = (rNumber) => {
+        const rowNumber = rNumber;
+        const columns = [];
+
+        return { rowNumber, columns };
+    }
+
+    const createColumn = (cNumber) => {
+        const columnNumber = cNumber;
+        const active = false;
+        const value = '';
+
+        const changeStatus = () => active = !active;
+
+        return { columnNumber, active, value, changeStatus };
+    }
+
+    const createGrid = function() {
+        for (let i = 0; i < 3; i++) {
+            const row = createRow(i);
+
+            for (let j = 0; j < 3; j++ ) {
+                const column = createColumn(j);
+                row.columns.push(column);
+            }
+
+            grid.push(row);
+        }
+    }();
+
+    return { grid };
+}();
+
+console.log(BOARD.grid);
